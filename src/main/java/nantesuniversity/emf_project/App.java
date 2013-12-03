9254bfef5f7a40a59150d54a3a106a0514a9dc70
Slayerxoxo <coraline.marie.cm@gmail.com>
2013-12-03 19:02:40 +0100

    Initialisation du projet

---
+++ src/main/java/nantesuniversity/emf_project/App.java
@@ -0,0 +1,13 @@
+package nantesuniversity.emf_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
